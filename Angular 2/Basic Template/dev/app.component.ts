4fdc89b49370d70b47dff24484274cd2fc14b43f
Chase <belax_8@hotmail.com>
2016-06-24 00:24:23 -0600

    initial commit of Basic Template

---
+++ Angular 2/Basic Template/dev/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <h1>Angular 2 Template</h1>
+        <p>Info</p>
+    `,
+})
+
+export class AppComponent {
+
+}

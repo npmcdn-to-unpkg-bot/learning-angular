91fb8fdb6d7fd384670d0ea362bac05623c6a6e7
Chase <belax_8@hotmail.com>
2016-06-27 23:02:21 -0600

    added routing

--- Angular 2/Tour of Heroes/app/hero.service.js
+++ Angular 2/Tour of Heroes/app/hero.service.js
@@ -8,13 +8,22 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require('@angular/core');
 var mock_heroes_1 = require('./mock-heroes');
+var core_1 = require('@angular/core');
 var HeroService = (function () {
     function HeroService() {
     }
     HeroService.prototype.getHeroes = function () {
         return Promise.resolve(mock_heroes_1.HEROES);
+    };
+    HeroService.prototype.getHeroesSlowly = function () {
+        return new Promise(function (resolve) {
+            return setTimeout(function () { return resolve(mock_heroes_1.HEROES); }, 2000);
+        });
+    };
+    HeroService.prototype.getHero = function (id) {
+        return this.getHeroes()
+            .then(function (heroes) { return heroes.filter(function (hero) { return hero.id === id; })[0]; });
     };
     HeroService = __decorate([
         core_1.Injectable(), 
--- Angular 2/Tour of Heroes/app/hero.service.ts
+++ Angular 2/Tour of Heroes/app/hero.service.ts
@@ -1,10 +1,22 @@
-import {Injectable} from '@angular/core';
+import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
+import {Injectable} from '@angular/core';
 
 @Injectable()
 
 export class HeroService {
   getHeroes() {
     return Promise.resolve(HEROES);
+  }
+
+  getHeroesSlowly() {
+    return new Promise<Hero[]>(resolve =>
+      setTimeout(() => resolve(HEROES), 2000)
+    );
+  }
+
+  getHero(id: number) {
+    return this.getHeroes()
+               .then(heroes => heroes.filter(hero => hero.id === id)[0]);
   }
 }
